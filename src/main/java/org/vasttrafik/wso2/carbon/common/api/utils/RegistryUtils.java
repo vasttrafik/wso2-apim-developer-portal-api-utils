4c1db7afffda0d4fde4f7667e7445cd479729c30
Daniel Oskarsson <daniel.oskarsson@gmail.com>
2015-11-23 14:39:24 +0100

    Add RegistryUtils

---
+++ src/main/java/org/vasttrafik/wso2/carbon/common/api/utils/RegistryUtils.java
@@ -0,0 +1,26 @@
+package org.vasttrafik.wso2.carbon.common.api.utils;
+
+import org.vasttrafik.wso2.carbon.identity.api.utils.ClientUtils;
+import org.wso2.carbon.registry.ws.stub.WSRegistryServiceStub;
+
+import javax.activation.DataHandler;
+import javax.ws.rs.NotFoundException;
+
+/**
+ * @author Daniel Oskarsson <daniel.oskarsson@gmail.com>
+ */
+public final class RegistryUtils {
+
+    private static final WSRegistryServiceStub registryStub = ClientUtils.getWSRegistryServiceStub();
+
+    public static Object getContent(final String path) {
+        try {
+            ClientUtils.authenticateIfNeeded(registryStub._getServiceClient());
+            final DataHandler datahandler = registryStub.getContent(path);
+            return datahandler.getContent();
+        } catch (Exception e) {
+            throw new NotFoundException();
+        }
+    }
+
+}
